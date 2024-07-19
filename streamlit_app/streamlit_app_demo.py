6ba6e8df9f7a8eb815cb1a479771c1b1c1a9105a
bennyocean <benjamin.schellinger@web.de>
2024-07-18 22:14:30 -0500

    add results streamlit file and some images, parquet files

--- streamlit_app/streamlit_app_demo.py
+++ streamlit_app/streamlit_app_demo.py
@@ -16,17 +16,18 @@ from imblearn.under_sampling import RandomUnderSampler
 import shap
 import joblib
 import gdown
+import os
+import sys
 
 
 # Streamlit UI
-st.title("LFB Response Time")
 st.sidebar.title("Table of Contents")
 pages = ["Home", "Data", "Model", "Results", "About"]
 page = st.sidebar.radio("Go to", pages)
 
 # Home
 if page == pages[0]:
-    st.write("### Home")
+    st.title("LFB Response Time")
     st.markdown("""
                 - Context / motivation / objectives
                 - Data / methods used
@@ -57,13 +58,89 @@ if page == pages[2]:
 
 # Results
 if page == pages[3]:
-    st.write("### Results")
+    st.title("Results Comparison & Interpretability")
     st.markdown("""
                 - Classification report (recall / prediciton)
                 - Interpretability (SHAP values, marginal contribution of features)
                 - limitations / contributions / future avenues
                 - Extra: deep-learning model
                 """)
+    # Ensure the tabs directory is in the Python path
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'tabs'))
+    import results
+    
+    st.subheader('Evaluation Summary')
+    
+    results.comp_table()
+    results.display_metrics_plot()
+    results.plot_recall_comp()
+
+    st.subheader("2. Prediction")
+    st.write('Some introduction..')
+    
+    # Make Predictions
+    y_pred, pca_feature_names, X_test_pca = results.prediction()
+    
+    # Load True Y Labels
+    y_true = results.load_y_test_pca()
+    
+    # Create Prediction DataFrame
+    X_test_pca_df = X_test_pca.copy()
+    X_test_pca_df.columns = pca_feature_names
+    X_test_pca_df['Prediction'] = y_pred
+
+    # Make Prediction Button
+    if st.button('Make Prediction'):
+        st.write("Prediction:")
+        st.dataframe(X_test_pca_df)
+
+
+    # Prediction Plot Dropdown
+    plot_type = st.selectbox(
+        "Choose a plot to display:",
+        ["Select Plot", "Confusion Matrix", "Prediction Distribution", "PCA Component Plot"]
+        )
+
+    if plot_type == "Confusion Matrix":
+        fig = results.plot_confusion_matrix(y_true, y_pred)
+        st.pyplot(fig)
+    elif plot_type == "Prediction Distribution":
+        fig = results.plot_pred_dist(y_pred)
+        st.pyplot(fig)
+    elif plot_type == "PCA Component Plot":
+        pc_x = st.selectbox("Select X-axis PC", pca_feature_names, index=0)
+        pc_y = st.selectbox("Select Y-axis PC", pca_feature_names, index=1)
+        fig = results.plot_pca_components(X_test_pca, pc_x, pc_y)
+        st.pyplot(fig)
+    else:
+        st.write("Please select a plot type.")
+
+
+    st.subheader("3. Interpretability")
+    st.write('Some introduction..')
+
+    # SHAP Summary Plots
+    shap_model_name = st.selectbox(
+        "Choose a model to display SHAP values:",
+        ["Select", "XGBoost", "Random Forest", "Logistic Regression"]
+        )
+
+    if shap_model_name != "Select":
+        fig = results.plot_shap_values(X_test_pca, pca_feature_names, shap_model_name)
+        st.pyplot(fig)
+
+
+    # Display mean_abs_shap_comparison
+    st.write('Some description..')
+    mean_abs_shap_comparison = results.plot_shap_across_models()
+    if mean_abs_shap_comparison:
+        st.image(mean_abs_shap_comparison, caption="Mean Absolute SHAP Values accros each model in the voting classifier", use_column_width=True)
+    else:
+        st.write("Default plot image not found.")
+
+
+
+
 
 # About
 if page == pages[4]:
---
+++ streamlit_app/tabs/results.py
@@ -0,0 +1,230 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
+from PIL import Image
+import shap
+import joblib
+import gdown
+import os
+
+# Results
+
+### 1. Evaluation Comparison
+### 1.1 Evaluation Metric Scores
+def comp_table():
+    data = {
+        'Model': [
+            'Random Forest', 
+            'XGBoost', 
+            'Voting Classifier (Soft)', 
+            'Voting Classifier (Hard)',
+            'Stacking Classifier'
+        ],
+        'Precision': [0.72, 0.73, 0.72, 0.72, 0.73],
+        'Recall': [0.70, 0.70, 0.69, 0.69, 0.70],
+        'F1-Score': [0.71, 0.71, 0.70, 0.70, 0.71],
+        'ROC AUC': [0.7352, 0.7411, 0.7364, 0, 0.7403],  # 0 Placeholder for Voting Classifier (Hard)
+        'Accuracy': [0.70, 0.6986, 0.6884, 0.6919, 0.6968],
+        'Balanced Accuracy': [0.6733, 0.6756, 0.6756, 0.6755, 0.6768]
+    }
+
+    comp = pd.DataFrame(data)
+    comp.set_index('Model', inplace=True)
+    st.write("Evaluation Comparison of Final Model Scores")
+    st.write('Some description..')
+    st.dataframe(comp)
+
+
+def display_metrics_plot():
+    st.write('Some description..')
+    
+    # Define the relative path where the images are stored
+    image_path = os.path.join(os.path.dirname(__file__), '..', 'assets', 'images', 'bs_evaluation_comparison')
+
+    # Dictionary to map score metrics to their respective image filenames
+    score_metrics = {
+        "Precision": "precision.png",
+        "Recall": "recall.png",
+        "F1-Score": "f1_score.png",
+        "ROC AUC Score": "roc_auc.png",
+        "Accuracy": "accuracy.png",
+        "Balanced Accuracy": "balanced_accuracy.png"
+    }
+
+    # Create a dropdown menu for selecting the score metric with a default option
+    options = ["Select a Score Metric"] + list(score_metrics.keys())
+    selected_metric = st.selectbox("Select a Score Metric:", options)
+
+    # Load and display the corresponding image if a valid option is selected
+    if selected_metric != "Select a Score Metric":
+        image_file = os.path.join(image_path, score_metrics[selected_metric])
+        image = Image.open(image_file)
+        st.image(image, caption=selected_metric, use_column_width=True)
+
+### 1.2 Recall Evaluation (focus on minority class)
+
+def plot_recall_comp():
+    st.write('Some explanation. Focus on Recall of minority class, i.e., target of 6 minutes not reached. Total test size: 307541. Majority class ~ 217298, minority class 90243. 2Goal to reduce false negatives and thus increase recall of minority class.')
+
+    recall_comp = {
+        'Model': [
+            'Random Forest', 
+            'XGBoost', 
+            'Voting Classifier (Soft)', 
+            'Voting Classifier (Hard)',
+            'Stacking Classifier'
+        ],
+        'Recall (Not Reached (>6 min))': [0.61, 0.62, 0.64, 0.64, 0.63],
+        'Recall (Reached (<=6 min))': [0.73, 0.73, 0.71, 0.72, 0.73]
+    }
+
+    recall_comp = pd.DataFrame(recall_comp)
+
+    # Find the model with the highest Recall (Not Reached (>6 min))
+    best_model = 'Voting Classifier (Hard)'
+
+    # Create the plot
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    bar_width = 0.35
+    index = range(len(recall_comp['Model']))
+
+    # Plot Recall (Not Reached (>6 min))
+    bars1 = ax.bar(index, recall_comp['Recall (Not Reached (>6 min))'], bar_width, label='Recall (Not Reached (>6 min))', color='lightcoral')
+
+    # Plot Recall (Reached (<=6 min))
+    bars2 = ax.bar([i + bar_width for i in index], recall_comp['Recall (Reached (<=6 min))'], bar_width, label='Recall (Reached (<=6 min))', color='lightgreen')
+
+    # Highlight the bar with the highest Recall (Not Reached (>6 min))
+    for i, bar in enumerate(bars1):
+        if recall_comp['Model'][i] == best_model:
+            bar.set_color('blue')
+
+    # Add labels and title
+    ax.set_xlabel('')
+    ax.set_ylabel('Recall')
+    ax.set_title('Recall Class Comparison')
+    ax.set_xticks([i + bar_width / 2 for i in index])
+    ax.set_xticklabels(recall_comp['Model'], rotation=45, ha='right')
+    ax.legend()
+
+    # Adjust y-axis limits
+    ax.set_ylim(0.55, 0.8)
+    
+    plt.xticks(rotation=45, ha='right')
+
+    plt.tight_layout()
+    save_path = os.path.join(os.path.dirname(__file__), '..', 'assets', 'images', 'bs_evaluation_comparison', 'recall_comparison.png')
+    plt.savefig(save_path)
+    st.pyplot(fig)
+
+    st.write('Some Conclusion..')
+
+
+### 2 Prediction
+### 2.1 Predicting Labels
+
+def load_X_test_pca():
+    parquet_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'X_test_pca.parquet')
+    X_test_pca = pd.read_parquet(parquet_path)
+    num_components = X_test_pca.shape[1]
+    pca_feature_names = [f'PC{i+1}' for i in range(num_components)]
+    return X_test_pca, pca_feature_names
+
+def load_y_test_pca():
+    parquet_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'y_test_pca.parquet')
+    y_test_pca = pd.read_parquet(parquet_path)
+    return y_test_pca
+
+def load_model():
+    model_path = os.path.join(os.path.dirname(__file__), '..', 'model.joblib')
+    model = joblib.load(model_path)
+    return model
+
+def prediction():
+    model = load_model()
+    X_test_pca, pca_feature_names = load_X_test_pca()
+    pred_values = model.predict(X_test_pca)
+    return pred_values, pca_feature_names, X_test_pca
+
+### 2.2 Prediction Plots
+
+def plot_pred_dist(y_pred):
+    fig, ax = plt.subplots()
+    ax.hist(y_pred, bins=[0, 1, 2], align='left', rwidth=0.8)
+    ax.set_xticks([0, 1])
+    ax.set_xticklabels(['Goal not Reached (>6 min)', ' Goal reached (<=6 min)'])
+    ax.set_xlabel('Predicted Class')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Distribution of Predictions')
+    return fig
+
+def plot_confusion_matrix(y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    fig, ax = plt.subplots()
+    disp.plot(ax=ax)
+    return fig
+
+def plot_pca_components(X_test_pca, pc_x, pc_y):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.scatterplot(data=X_test_pca, x=pc_x, y=pc_y, hue='Prediction', palette='Set1', ax=ax)
+    ax.set_title(f'PCA Component Plot: {pc_x} vs {pc_y}')
+    return fig
+
+
+### 3 Interpretability
+### 3.1 Mean Shap Value Calculation & Plot
+
+def get_shap_values(model, X, model_type='tree'):
+    if model_type == 'tree':
+        explainer = shap.TreeExplainer(model)
+    elif model_type == 'linear':
+        explainer = shap.LinearExplainer(model, X)
+    else:
+        explainer = shap.KernelExplainer(model.predict, X)
+    shap_values = explainer.shap_values(X)
+    return shap_values, explainer
+    
+def plot_shap_values(X_test_pca, pca_feature_names, model_name):
+    model = load_model()
+    if model_name == 'XGBoost':
+        fitted_model = model.named_estimators_['XGboost']
+        model_type = 'tree'
+    elif model_name == 'Random Forest':
+        fitted_model = model.named_estimators_['RF']
+        model_type = 'tree'
+    elif model_name == 'Logistic Regression':
+        fitted_model = model.named_estimators_['LogReg']
+        model_type = 'linear'
+    else:
+        return None
+
+    shap_values, explainer = get_shap_values(fitted_model, X_test_pca, model_type)
+    
+    plt.figure()
+    shap.summary_plot(shap_values, X_test_pca, feature_names=pca_feature_names, plot_type="bar", show=False)
+    plt.title(f'SHAP Summary Plot for {model_name}')
+    return plt.gcf()
+
+### 3.2 Mean Shap Value Comparison in the Voting Classifier
+
+def plot_shap_across_models():
+    mean_abs_shap_comparison = os.path.join(os.path.dirname(__file__), '..', 'assets', 'images', 'bs_shap_values', 'mean_abs_shap_comparison.png')
+    if os.path.exists(mean_abs_shap_comparison):
+        default_plot_image = Image.open(mean_abs_shap_comparison)
+        return default_plot_image
+    else:
+        return None    
+
+### 3.3 SHAP Violin Plots for for Each Model
+
+### 3.4 Average SHAP Violin Plot for Voting Classifier
+
+### 3.5 Local Interpretation for one Observation
+
+### 3.6 Interactive Plot for first 1000 Observations
