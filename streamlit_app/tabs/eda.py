f9afc9af07acca19ef1936dd5907f402cd21bf60
izzy <ismarahmaier@gmail.com>
2024-07-22 11:58:40 +0200

    create main.py to load tabs and first draft of eda page

---
+++ streamlit_app/tabs/eda.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import FuncFormatter
+from PIL import Image
+from pathlib import Path
+
+def show_data_exploration(data_path, image_path):
+    # Streamlit UI
+    st.title("Exploratory Data Analysis")
+
+    st.markdown("""
+                ### Data Sets
+                - **LFB Incident Dataset**: incidents handled since January 2009
+                    - *39 columns, 716,551 rows*
+                - **LFB Mobilisation Dataset**: fire engines dispatched to incidents
+                    - *22 columns, 2,373,348 rows*
+                - **Google Maps**: locations of fire stations in London
+                - **Bank Holidays Dataset**: all bank holidays in London
+                
+                ### Feature Engineering
+                - **From maps data**: the distance of the incident to the nearest fire station
+                - **From geo data**: mapped London into a cell grid
+                - **From bank holidays**: created a boolean feature indicating if the incident happened on a holiday
+                - **From date column**: extracted if the incident occurred on a weekend, time of day, and weekday
+                
+                ### Calculate Target Variable
+                - Converted time of call and time of arrival to datetime format
+                - Calculated the difference in seconds to get the total response time
+                
+                ### Removing Irrelevant Data
+                - Focused only on the first pump arriving at the incident
+                - Removed several columns not relevant to the target variable
+                - Removed NaNs
+                - After merging datasets, feature engineering, and removing variables:
+                    - *320 columns, 1,537,704 rows*
+                    
+                ### Preparing Data for Modeling
+                - Used cyclic encoding for the time-related features to ensure correct interpretation by the model
+                - Transformed the variables *DistanceToStation* and *TotalResponseTime* into logarithmic form to ensure a normal distribution
+    """)
+    data_path = Path(__file__).resolve().parents[2] / 'data' / 'dataframe_compressed.parquet'
+
+    df = pd.read_parquet(data_path)
+    df['Year'] = df['DateTimeCall'].dt.year
+    df = df[df['Year'] != 2024]
+    average_response_time_per_year = df.groupby('Year')['TotalResponseTime'].mean().reset_index()
+
+    st.title('Data Visualization')
+
+    image = Image.open("../img/average_total_response_time.png")
+    st.image(image, caption='Average Total Response Time Over the Years', use_column_width=True)
